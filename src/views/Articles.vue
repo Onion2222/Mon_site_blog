cb2b2261aa194e11a7e0b32ac28cf18b81647fbd
Onion2222 <arthurperrin.22@gmail.com>
2021-06-06 18:31:06 +0200

    corrections affichage article

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -1,8 +1,15 @@
 <template>
-<div>
-    <iframe  id="myIframe" width="90%"
-    :src="`../Articles/${this.$route.params.path}/index.html`"
-    />
+<div id=mainArticle>
+  <aside>
+    <button>retour</button>
+    <button>partager</button>
+  </aside>
+
+  <div id="myIframeDiv">
+      <iframe  id="myIframe"  scrolling="no"
+      :src="`../Articles/${this.$route.params.path}/index.html`"
+      />
+  </div>
 </div>
 </template>
 
@@ -26,7 +33,7 @@ export default {
           var iframe = document.getElementById("myIframe");
           
           iframe.onload = function(){
-            iframe.style.height = (iframe.contentWindow.document.body.scrollHeight+30) + 'px';
+            iframe.style.height = (iframe.contentWindow.document.body.scrollHeight+100) + 'px';
           }
             
         },
@@ -36,6 +43,29 @@ export default {
 </script>
 
 <style lang="scss">
+
+#mainArticle{
+  display: flex;
+  flex-direction: row;
+  border: 1px dotted green;
+}
+#myIframeDiv{
+  width:100%;
+  border: 1px dotted blue;
+}
+
+iframe{
+  
+  display: block;
+  width:90%;
+  right:0;
+  border: 1px dotted red;
+}
+aside{
+  width:10%;
+  display: flex;
+  flex-direction: column;
+}
 
 
 </style>
